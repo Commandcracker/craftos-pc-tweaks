5f1b7838ed8c67c5f7c82c59bab47a34e321fc95
Commandcracker <lukas.Fricke.dev@gmail.com>
2023-06-05 22:13:11 +0200

    Add redirect_single

---
+++ lua/redirect_single.lua
@@ -0,0 +1,15 @@
+-- This module will make shure,
+-- that all terminal output will be
+-- displayed in one terminal.
+
+function term.current()
+    return term
+end
+
+function term.native()
+    return term
+end
+
+function term.redirect()
+    return term
+end
